96fa309d1d3a52ae2715fb5b80337b7772e20b8d
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2018-11-26 15:13:38 +0300

    create auth0, firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+ var firebase = require("firebase/app");
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD2Lp_QC8a4jWHsy6GOZdN0sWjXLLihXy8",
+    authDomain: "anilibria-1e924.firebaseapp.com",
+    databaseURL: "https://anilibria-1e924.firebaseio.com",
+    projectId: "anilibria-1e924",
+    storageBucket: "anilibria-1e924.appspot.com",
+    messagingSenderId: "793792578918"
+  };
+  firebase.initializeApp(config);
