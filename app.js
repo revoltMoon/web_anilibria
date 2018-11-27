4270ed010c3eb44544d3ba7a6f0614bb1b5a1ae4
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2018-11-27 12:01:59 +0300

    working authorization + hosted on now.sh

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ window.addEventListener('load', function() {
   content.style.display = 'block';
   loadingSpinner.style.display = 'none';
 
-  var webAuth = new auth0.WebAuth({
-    domain: 'revoltmoon.eu.auth0.com',
-    clientID: 'tzwQhlg0V3yzySAQe92rADhvpDees0oW',
-    redirectUri: 'http://localhost:8080',
-    responseType: 'token id_token',
-    scope: 'openid',
-    leeway: 60
-  });
-
   var loginStatus = document.querySelector('.container h4');
   var loginView = document.getElementById('login-view');
   var homeView = document.getElementById('home-view');
@@ -21,6 +12,11 @@ window.addEventListener('load', function() {
   var homeViewBtn = document.getElementById('btn-home-view');
   var loginBtn = document.getElementById('qsLoginBtn');
   var logoutBtn = document.getElementById('qsLogoutBtn');
+    
+  var lock = new Auth0Lock(
+  'tzwQhlg0V3yzySAQe92rADhvpDees0oW',
+  'revoltmoon.eu.auth0.com'
+  );
 
   homeViewBtn.addEventListener('click', function() {
     homeView.style.display = 'inline-block';
@@ -28,11 +24,37 @@ window.addEventListener('load', function() {
   });
 
   loginBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    webAuth.authorize();
+    lock.show()
   });
 
   logoutBtn.addEventListener('click', logout);
+
+
+
+  function isAuthenticated() {
+  // Check whether the current time is past the
+  // access token's expiry time
+  var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+  return new Date().getTime() < expiresAt;
+  }
+  
+  function displayButtons() {
+  if (isAuthenticated()) {
+  loginBtn.style.display = 'none';
+  logoutBtn.style.display = 'inline-block';
+  } else {
+    loginBtn.style.display = 'inline-block';
+    logoutBtn.style.display = 'none';
+    }
+  }
+
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    displayButtons();
+  }
 
   function setSession(authResult) {
     // Set the time that the access token will expire at
@@ -42,53 +64,22 @@ window.addEventListener('load', function() {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+    displayButtons()
   }
-
-  function logout() {
-    // Remove tokens and expiry time from localStorage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-    // displayButtons();
-  }
-
-  function isAuthenticated() {
-    // Check whether the current time is past the
-    // access token's expiry time
-    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
-  }
-
-  function handleAuthentication() {
-    webAuth.parseHash(function(err, authResult) {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
-        setSession(authResult);
-        loginBtn.style.display = 'none';
-        homeView.style.display = 'inline-block';
-      } else if (err) {
-        homeView.style.display = 'inline-block';
-        console.log(err);
-        alert(
-          'Error: ' + err.error + '. Check the console for further details.'
-        );
+  
+// Listening for the authenticated event
+  lock.on("authenticated", function(authResult) {
+    // Use the token in authResult to getUserInfo() and save it to localStorage
+    lock.getUserInfo(authResult.accessToken, function(error, profile) {
+      if (error) {
+        // Handle error
+        return;
       }
-      //displayButtons();
+      setSession(authResult)
+      //document.getElementById('nick').textContent = profile.nickname;
+
+      //localStorage.setItem('accessToken', authResult.accessToken);
+      //localStorage.setItem('profile', JSON.stringify(profile));
     });
-  }
-
-  function displayButtons() {
-    if (isAuthenticated()) {
-      loginBtn.style.display = 'none';
-      logoutBtn.style.display = 'inline-block';
-      loginStatus.innerHTML = 'You are logged in!';
-    } else {
-      loginBtn.style.display = 'inline-block';
-      logoutBtn.style.display = 'none';
-      loginStatus.innerHTML =
-        'You are not logged in! Please log in to continue.';
-    }
-  }
-
-  handleAuthentication();
+  });
 });
